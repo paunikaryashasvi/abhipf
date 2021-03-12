c6e762539f990290d26758fa3a0e9fb78f892d8c
paunikaryashasvi <65233554+paunikaryashasvi@users.noreply.github.com>
2021-03-12 16:13:34 +0530

    initial commit

---
+++ myportfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// components
+import  MyNavbar  from "./components/my-navbar/mynavbar.component";
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <MyNavbar/>
+      my portfolio
+    </div>
+  );
+}
+
+export default App;
